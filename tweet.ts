05675875785a235f56dba65a6ac729d081a8dc3d
skmtrd <skmtgame23@gmail.com>
2024-12-11 22:35:39 +0900

    ほぼ

--- tweet.ts
+++ tweet.ts
@@ -1,5 +1,7 @@
 import { TwitterApi } from "twitter-api-v2";
 import dotenv from "dotenv";
+import { ApiResponse, Assignment } from "./types";
+import { parseISO, compareAsc } from "date-fns";
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ const rwClient = client.readWrite;
 
 const tweet = async (message: string) => {
   try {
+    const formattedMessage = message.replace(/\\n/g, "\n").trim();
+
     await rwClient.v2.tweet(message);
     console.log("ツイートが投稿されました:", message);
   } catch (error) {
@@ -21,5 +25,65 @@ const tweet = async (message: string) => {
   }
 };
 
-const now_date = new Date();
-const message = `ただいまの時刻は${now_date.getHours()}時${now_date.getMinutes()}分です。`;
+const formatDeadlineNotifications = (assignments: Assignment[]): string => {
+  if (assignments.length === 0) {
+    return "期限が近い課題はありません";
+  }
+
+  const header = "提出期限まで一時間を切っている課題があります\n";
+
+  const formattedAssignments = assignments
+    .map(
+      (assignment, index) => `
+  ${index + 1}. ${assignment.title}
+  ⏰ 期限: ${assignment.deadLine}
+  ────────────`
+    )
+    .join("\n");
+
+  return `${header}${formattedAssignments}`.trim();
+};
+
+export const fetchAssignments = async (): Promise<Assignment[]> => {
+  try {
+    const response = await fetch("https://iniad-sns.vercel.app/api/assignment");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data: ApiResponse = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error;
+  }
+};
+
+function convertTime(dateStr: string) {
+  const date = new Date(dateStr.replace("/", "T"));
+  const JSTDate = new Date(date.getTime() + 9 * 60 * 60 * 1000)
+    .toISOString()
+    .replace("Z", "+09:00");
+
+  return parseISO(JSTDate);
+}
+
+const main = async () => {
+  const assignments = await fetchAssignments();
+  const now = new Date();
+  const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
+  console.log(now);
+  console.log(oneHourLater);
+
+  const filteredAssignments = assignments.filter((assignment) => {
+    const deadLineDate = convertTime(assignment.deadLine);
+    return deadLineDate >= now && deadLineDate <= oneHourLater;
+  });
+
+  if (filteredAssignments.length === 0) {
+    return;
+  }
+
+  await tweet(formatDeadlineNotifications(filteredAssignments));
+};
+
+main();
